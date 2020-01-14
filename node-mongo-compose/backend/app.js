56f39d84fd279cd1cecfaab72f4852436a120ca3
eduardo cipriano <edfcsx@gmail.com>
2020-01-13 21:30:56 -0300

    files for end course

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const restful = require('node-restful')
 const server = express()
-const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-//Database
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(cors())
+
+// Database
+const mongoose = restful.mongoose
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
+
+// ODM
+const Client = restful.model('Client', {
+  name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({ new: true, runValidators: true })
+
+// Routers
+Client.register(server, '/clients')
+
 
 server.get('/', (req, res, next) => {
   res.send('Backend!')
